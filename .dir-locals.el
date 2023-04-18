0b68a38c9ad935facba5e8fddca85318cce25b55
eapiova <eapiova@gmail.com>
2023-04-18 16:51:46 +0200

    update

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-((coq-mode
-  . ((eval .
-	   (progn
-	     (make-local-variable 'coq-prog-name)
-             (setq coq-prog-name "hoqtop")
-	     (make-local-variable 'coq-prog-args)
-	     (setq coq-prog-args `("-emacs" "-indices-matter"
-				  "-Q" ,(expand-file-name (locate-dominating-file buffer-file-name ".dir-locals.el")) "" )))))))
